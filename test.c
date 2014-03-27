618691abaa9b3534c94ac4067a3ebd9b0d5ec6e6
Sebastian Lausberg <laus0006@flinders.edu.au>
2014-03-27 15:03:55 +1030

    Added tests for slide_to_left function

--- test.c
+++ test.c
@@ -32,9 +32,29 @@ int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
   return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_left);
 }
 
+int ttl_vector_slide_left(int i1,int i2,int i3,int i4,char *msg,
+               int o1,int o2,int o3,int o4)
+{
+  return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,slide_tiles_to_left);
+}
+
+int test_slide_left(){
+  int e=0;
+  e|=ttl_vector_slide_left(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
+  e|=ttl_vector_slide_left(1,0,0,0,"Value on left stays on left after shift",1,0,0,0);
+  e|=ttl_vector_slide_left(0,0,0,1,"Value on right shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector_slide_left(0,0,1,0,"Value in (right) middle shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector_slide_left(0,1,0,0,"Value in (left) middle shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector_slide_left(2,0,2,0,"non-1 value tiles shift left",2,2,0,0);
+  e|=ttl_vector_slide_left(2,0,2,8,"Alternate value tiles stay in order",2,2,8,0);
+  e|=ttl_vector_slide_left(2,0,2,0,"Value in (left) middle shifts to left edge after shift",2,2,0,0);
+  return e;
+}
+
 int test_tilt_left()
 {
   int e=0;
+  
   e|=ttl_vector(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
   e|=ttl_vector(1,0,0,0,"Value on left stays on left after shift",1,0,0,0);
   e|=ttl_vector(0,0,0,1,"Value on right shifts to left edge after shift",1,0,0,0);
@@ -51,6 +71,7 @@ int test_tilt_left()
 int main(int argc,char **argv)
 {
   int e=0;
+  e|=test_slide_left();
   e|=test_tilt_left();
   return e;
 }
